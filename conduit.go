c81c19216a5d63ac798675a6e49f72294c5d0fc7
Tim Peoples <tim@toolman.org>
2018-03-13 15:19:03 -0700

    Add preamble to each source file

--- conduit.go
+++ conduit.go
@@ -1,3 +1,18 @@
+// Copyright © 2018 Timothy E. Peoples <eng@toolman.org>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 // Package conduit provides the Conduit type used for transferring open file
 // descriptors between cooperating processes. Common use cases for this
 // behavior would be to transfer ownership of an open file from one process to
